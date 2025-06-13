538a25bfe1136b030bc47dc73ece090671f2536a
Kartheek <kartheek200@gmail.com>
2025-06-14 00:43:55 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,13 +40,13 @@ export const Navbar = () => {
         <div className="hidden md:flex space-x-8">
           {navItems.map((navItem, key) => {
             return (
-              <a
+              <Link
                 key={key}
-                href={navItem.href}
+                to={navItem.href}
                 className="text-foreground/80 hover:text-primary transition-colors duration-300"
               >
                 {navItem.name}
-              </a>
+              </Link>
             );
           })}
           <ThemeToggle />
